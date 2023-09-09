33f09175f8fb3fb854795c270bd61e6dac5387e4
Kona <landon.deroin@gmail.com>
2023-09-08 20:32:04 -0400

    bug chasing

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -16,7 +16,7 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   const db = await openDB("jate", 1);
-  const text = indexedDB.transaction("jate", "readwrite");
+  const text = db.transaction("jate", "readwrite");
   const store = text.objectStore("jate");
   await store.put({ content });
   await text.done;
@@ -27,11 +27,9 @@ export const putDb = async (content) => {
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   const db = await openDB("jate", 1);
-  const txt = db.transaction('jate', "readonly");
-  const store = txt.objectStore('jate')
-  const request = store.getAll();
-  console.log("All text from database retrieved", request);
-  return allText;
+  const allText = await db.getAll("jate");
+  console.log("all text from database retrieved", allText);
+  return allText.length ? allText[allText.length - 1].content : null;
 };
 
 initdb();
--- client/src/js/editor.js
+++ client/src/js/editor.js
@@ -25,12 +25,20 @@ export default class {
     // When the editor is ready, set the value to whatever is stored in indexeddb.
     // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
     getDb().then((data) => {
-      console.info("Loaded data from IndexedDB, injecting into editor");
-      this.editor.setValue(data || localData || header);
-    });
-
-    this.editor.on("change", () => {
-      localStorage.setItem("content", this.editor.getValue());
+      console.info("Loaded data from IndexedDB:", data);
+      
+      if (typeof data === 'string') {
+        this.editor.setValue(data);
+      } else if (localData) {
+        console.warn("Data from IndexedDB wasn't a string. Using localData.");
+        this.editor.setValue(localData);
+      } else {
+        console.warn("Using header as default content.");
+        this.editor.setValue(header);
+      }
+    }).catch(err => {
+      console.error("Error fetching data from IndexedDB:", err);
+      this.editor.setValue(header);  // Use header as a fallback in case of errors.
     });
 
     // Save the content of the editor when the editor itself is loses focus
