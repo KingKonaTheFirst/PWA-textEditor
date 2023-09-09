8c0db4892860f790218ddd82ac1a802e206e929c
Kona <landon.deroin@gmail.com>
2023-09-08 20:24:50 -0400

    tried fixing saving

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,34 +1,37 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
+import { header } from "./header";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  const db = await openDB('jate', 1);
-  const text = db.transaction('jate', "readwrite");
-  const store = text.objectStore('jate');
+  const db = await openDB("jate", 1);
+  const text = indexedDB.transaction("jate", "readwrite");
+  const store = text.objectStore("jate");
   await store.put({ content });
   await text.done;
 
   console.log("content added to database");
-}
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  const db = await openDB('jate', 1);
-  const allText = await db.getAll('jate');
-  console.log('All text from database retrieved', allText);
+  const db = await openDB("jate", 1);
+  const txt = db.transaction('jate', "readonly");
+  const store = txt.objectStore('jate')
+  const request = store.getAll();
+  console.log("All text from database retrieved", request);
   return allText;
-}
+};
 
 initdb();
